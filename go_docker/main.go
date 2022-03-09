1094d91dd88d174bdf48c46e054cedd4d5813d21
JoeV <15126922+josephedward@users.noreply.github.com>
2022-03-09 15:38:50 -0500

    docker

---
+++ go_docker/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/afdolriski/golang-docker/database"
+	"github.com/gin-gonic/gin"
+)
+
+// inside func main
+
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/animal/:name", func(c *gin.Context) {
+		animal, err := database.GetAnimal(c.Param("name"))
+		if err != nil {
+		  c.String(404, err.Error())
+		  return
+		}
+		c.JSON(200, animal)
+	  })
+	r.Run(":3000")
+}
